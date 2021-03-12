5e34a7147b43603a84fe80322f23af17b7fd4c56
杜辛 <761082014@qq.com>
2021-03-12 21:34:12 +0800

    支付模块基本配置构建完成

---
+++ miujoke-provider-payment8001/src/main/java/com/miujoke/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.miujoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: TODO
+ * @Author: miujoke
+ * @DateTime: 2021/3/12 21:18
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+       SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
