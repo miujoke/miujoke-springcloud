7e0a8b2d57fcd80fe9d60a71eaa9e5703d7fc837
杜辛 <761082014@qq.com>
2021-03-14 21:30:03 +0800

    添加eureka7002 模块 集群 eureka7001与eureka7002  相互注册，相互守望（eureka7001中注册eureka7002；eureka7002中注册eureka7001）.这样就能实现集群

---
+++ miujoke-eureka-server7002/src/main/java/com/miujoke/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.miujoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description: TODO
+ * @Author: miujoke
+ * @DateTime: 2021/3/14 21:22
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
