45f76f522b5bf4b0b3c1517addf99c26b2553c6c
杜辛 <761082014@qq.com>
2021-03-14 22:02:58 +0800

    因为集群支付业务模块不知道是8001还是8002，所以在controller中获取模块的端口号即可辨别是调用那一台服务器

--- miujoke-provider-payment8002/src/main/java/com/miujoke/controller/PaymentController.java
+++ miujoke-provider-payment8002/src/main/java/com/miujoke/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.miujoke.entities.CommonResult;
 import com.miujoke.entities.Payment;
 import com.miujoke.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -22,28 +23,29 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
 
     @PostMapping("/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("*****插入结果" + result);
         if (result > 0) {
-            return new CommonResult(200, "插入成功", result);
+            return new CommonResult(200, "插入成功" + serverPort, result);
         } else {
-            return new CommonResult(444, "插入失败");
+            return new CommonResult(444, "插入失败" + serverPort);
         }
     }
 
     @GetMapping("/getPaymentById/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment result = paymentService.getPaymentById(id);
-        log.info("*****插入结果" + result+"\t"+"哈哈哈");
+        log.info("*****插入结果" + result + "\t" + "哈哈哈");
         if (result != null) {
-            return new CommonResult(200, "查询成功了222", result);
+            return new CommonResult(200, "查询成功了222" + serverPort, result);
         } else {
-            return new CommonResult(444, "没有对应记录", null);
+            return new CommonResult(444, "没有对应记录" + serverPort, null);
         }
     }
-
 
 }
