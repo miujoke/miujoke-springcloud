c0cc39b7c7407233f4fd87d89ec8d53528ab089d
杜辛 <761082014@qq.com>
2021-03-14 20:53:50 +0800

    将 支付模块 payment8001  注册到 Eureka

--- miujoke-provider-payment8001/src/main/java/com/miujoke/PaymentMain8001.java
+++ miujoke-provider-payment8001/src/main/java/com/miujoke/PaymentMain8001.java
@@ -3,6 +3,7 @@ package com.miujoke;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description: TODO
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.miujoke.dao")
+@EnableEurekaClient  // 开启Eureka客服端
 public class PaymentMain8001 {
     public static void main(String[] args) {
        SpringApplication.run(PaymentMain8001.class,args);
