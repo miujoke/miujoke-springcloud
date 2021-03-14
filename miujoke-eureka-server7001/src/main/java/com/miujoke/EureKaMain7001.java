b019d4c3dd1b0e13933fb7455a0a68a7582825fb
杜辛 <761082014@qq.com>
2021-03-14 20:34:05 +0800

    Eureka单机版模块

---
+++ miujoke-eureka-server7001/src/main/java/com/miujoke/EureKaMain7001.java
@@ -0,0 +1,18 @@
+package com.miujoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description: TODO
+ * @Author: miujoke
+ * @DateTime: 2021/3/14 20:21
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EureKaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EureKaMain7001.class,args);
+    }
+}
