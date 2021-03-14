199af1a70f9f2916784941d2563c1832cc48cadb
杜辛 <761082014@qq.com>
2021-03-14 21:00:47 +0800

    将 消费模块 order80  注册到 Eureka

--- miujoke-consumer-order80/src/main/java/com/miujoke/OrderMain80.java
+++ miujoke-consumer-order80/src/main/java/com/miujoke/OrderMain80.java
@@ -2,6 +2,7 @@ package com.miujoke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description: TODO 客服端
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @DateTime: 2021/3/14 15:32
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
