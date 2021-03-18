3b274a33b12eec149f171fd5e1105a34e7e8c8db
杜辛 <761082014@qq.com>
2021-03-18 20:42:10 +0800

    集成zookeeper并测试成功

---
+++ miujoke-provider-payment8004/src/main/java/com/miujoke/PaymentMain8004.java
@@ -0,0 +1,19 @@
+package com.miujoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description: TODO
+ * @Author: miujoke
+ * @DateTime: 2021/3/18 20:28
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
+
