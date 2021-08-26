0fc2f87025d2bdeadd9a47c074cdc7d2588305f3
杜辛 <761082014@qq.com>
2021-08-26 13:39:35 +0800

    直连型交换机-》当多个监听器同时监听一个队列时，是通过轮询的方式进行消费的

---
+++ miujoke-mq-consumer/src/main/java/com/miujoke/mqListener/DirectReceiver1.java
@@ -0,0 +1,21 @@
+package com.miujoke.mqListener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @Author: miujoke
+ * @DateTime: 2021/8/26 13:31
+ */
+@Component
+// 监听的队列名称 TestDirectQueue
+@RabbitListener(queues = "TestDirectQueue")
+public class DirectReceiver1 {
+    @RabbitHandler
+    public void process(Map message){
+        System.out.println("01A-DirectReceiver消费者收到消息  : " + message.toString());
+    }
+}
