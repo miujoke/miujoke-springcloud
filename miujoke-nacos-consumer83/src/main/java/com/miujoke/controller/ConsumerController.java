ddebd51d54a8c54503103be7380bef6e79123a50
username <miujoke@gmail.com>
2023-05-23 22:21:41 +0800

    nacos注册服务一会儿失效原因解决，解决引入多余jar包导致，直接将多余jar包移除即可

--- miujoke-nacos-consumer83/src/main/java/com/miujoke/controller/ConsumerController.java
+++ miujoke-nacos-consumer83/src/main/java/com/miujoke/controller/ConsumerController.java
@@ -1,6 +1,6 @@
 package com.miujoke.controller;
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +15,6 @@ import javax.annotation.Resource;
  */
 
 @RestController
-@Slf4j
 public class ConsumerController {
 
     @Resource
